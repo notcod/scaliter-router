ed73104ecc5bf85827cda1e722862f48db97796f
notcod <mneca99+git@gmail.com>
2023-09-28 00:23:52 +0200

    router

--- src/Router.php
+++ src/Router.php
@@ -4,7 +4,8 @@ namespace Scaliter;
 
 class Router
 {
-    public $Request, $Controller, $Method, $Params;
+    public $Request, $Controller, $Method = NULL;
+    public array $Params = [];
     private static array $_ROUTES = [
         'Controller' => [
             '/' => 'HomeController', 'index'
@@ -13,22 +14,22 @@ class Router
 
     public static function view(string $url, array $call)
     {
-        self::$_ROUTES['Controller'][$url] = $call;
+        self::$_ROUTES['Controller'][$url]  = $call;
     }
     public static function get(string $url, array $call)
     {
-        self::$_ROUTES['Response'][$url] = $call;
+        self::$_ROUTES['Response'][$url]    = $call;
     }
     public static function post(string $url, array $call)
     {
-        self::$_ROUTES['Request'][$url] = $call;
+        self::$_ROUTES['Request'][$url]     = $call;
     }
 
     public function __construct(array $server)
     {
-        $HTTP_CONTENT_HASH  = $server['HTTP_CONTENT_HASH'] ?? '';
-        $REQUEST_METHOD     = $server['REQUEST_METHOD'] ?? '';
-        $REQUEST_URI        = $server['REQUEST_URI'] ?? '';
+        $HTTP_CONTENT_HASH  = $server['HTTP_CONTENT_HASH']  ?? '';
+        $REQUEST_METHOD     = $server['REQUEST_METHOD']     ?? '';
+        $REQUEST_URI        = $server['REQUEST_URI']        ?? '';
 
         $this->Request = $this->request($REQUEST_METHOD, $HTTP_CONTENT_HASH);
 
@@ -40,7 +41,7 @@ class Router
             $Controller   = $mapper[0];
             $Method       = $mapper[1];
 
-            if (method_exists($Controller, $Method)) {
+            if (class_exists($Controller) && method_exists($Controller, $Method)) {
                 $this->Controller   = $mapper[0];
                 $this->Method       = $mapper[1];
                 $this->Params       = $mapper[2];
@@ -62,14 +63,14 @@ class Router
     private function mapper(string $request, string $uri, array $routes = [])
     {
         $params = array_diff(explode('/', $uri), array(""));
-        if ($controller = $routes[$uri] ?? NULL) {
-            $class  = $controller[0] ?? NULL;
-            $method = $controller[1] ?? NULL;
+        if ($req = $routes[$uri] ?? NULL) {
+            $class  = $req[0] ?? NULL;
+            $method = $req[1] ?? NULL;
             $params = [];
-        } elseif ($controller = $this->params_try($params, $routes)) {
-            $class  = $controller[0] ?? NULL;
-            $method = $controller[1] ?? NULL;
-            $params = $controller[2] ?? [];
+        } elseif ($req = $this->params_try($params, $routes)) {
+            $class  = $req[0] ?? NULL;
+            $method = $req[1] ?? NULL;
+            $params = $req[2] ?? [];
         } else {
             $class  = current($params) != NULL ? ucfirst(strtolower(array_shift($params))) . $request : NULL;
             $method = array_shift($params);
@@ -82,12 +83,12 @@ class Router
         $params_try = [];
         while (count($params) >= 1) {
             $route_try = '/' . implode('/', $params) . '/';
-            $controller = $routes[$route_try] ?? false;
-            if (!$controller) {
+            $req = $routes[$route_try] ?? false;
+            if (!$req) {
                 $params_try[] = array_pop($params);
             } else {
-                $class = $controller[0] ?? NULL;
-                $method = $controller[1] ?? NULL;
+                $class = $req[0] ?? NULL;
+                $method = $req[1] ?? NULL;
                 return [$class, $method, $params_try];
             }
         }
